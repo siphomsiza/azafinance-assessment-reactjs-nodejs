63872b6820f3fdd3760647c3cfe814f191bfc033
siphomsiza <siphosmall@gmail.com>
2021-12-03 15:42:53 +0200

    fixing for heroku

--- src/pages/artist/artist.component.jsx
+++ src/pages/artist/artist.component.jsx
@@ -40,12 +40,11 @@ const Artist = (props) => {
         dispatch({
             type: "SEARCH_PLAYLIST_REQUEST"
         });
-        //fetch(`https://cors-anywhere.herokuapp.com/http://api.deezer.com/artist/462812/top?limit=50`)
         fetch(`https://cors-anywhere.herokuapp.com/http://api.deezer.com/${artist.tracklist.substring(22, artist.tracklist.length)}`)
         //fetch(`${artist.tracklist.substring(22, artist.tracklist.length)}`)
             .then(response => response.json())
             .then(data => {
-
+                console.log(data) 
                 if (data.data) {
                     dispatch({
                         type: "SEARCH_PLAYLIST_SUCCESS",
@@ -62,7 +61,6 @@ const Artist = (props) => {
 
 
     const { albums, errorMessage, loading, playlist } = state;
-
     const artistAlbums = albums.map((item, index) => {
         const formattedAlbum = {
             artist: item.artist,
